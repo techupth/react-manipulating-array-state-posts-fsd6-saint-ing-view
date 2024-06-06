8bcde28fcb0e200d1d35260d9cefe093efbb299d
Wongsathon Chuijit <wongsathon@Wongsathons-MacBook-Air.local>
2024-06-06 16:33:41 +0700

    feat:done exs

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,28 +1,67 @@
+import { useState } from "react";
+
 function Posts() {
+  const initialPosts = [
+    { title: "Paper Clips", likes: 61 },
+    { title: "Born to Kill", likes: 46 },
+    { title: "Ten Shrews, The (Kymmenen riivinrautaa)", likes: 50 },
+    { title: "Ciel est Ã  vous, Le (Woman Who Dared, The)", likes: 3 },
+    { title: "Tigger Movie, The", likes: 66 },
+    { title: "Cat People", likes: 70 },
+    { title: "Brooklyn Castle", likes: 46 },
+    { title: "Snow Angels", likes: 1 },
+    { title: "Rose Tattoo, The", likes: 20 },
+    { title: "It's A Wonderful World", likes: 6 },
+  ];
+
+  const [posts, setPosts] = useState(initialPosts);
+
+  const handleLike = (index) => {
+    const newPosts = [...posts];
+    newPosts[index].likes += 1;
+    setPosts(newPosts);
+  };
+  const handleDislike = (index) => {
+    const newPosts = [...posts];
+    if (newPosts[index].likes >= 1) {
+      newPosts[index].likes -= 1;
+      setPosts(newPosts);
+    }
+  };
+
   return (
-    <div class="app-wrapper">
-      <h1 class="app-title">Posts</h1>
-      <div class="post-list">
-        <div class="post-item">
-          <div class="post-header">
-            <h2>Post Title #1</h2>
-            <div class="post-social-media-stats">
-              <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
+    <div className="app-wrapper">
+      <h1 className="app-title">Posts</h1>
+      <div className="post-list">
+        {posts.map((post, index) => (
+          <div key={index} className="post-item">
+            <div className="post-header">
+              <h2>{post.title}</h2>
+              <div className="post-social-media-stats">
+                <span className="stats-topic">Likes: </span>
+                <span className="post-likes">{post.likes}</span>
+              </div>
+            </div>
+            <p className="post-content">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+              vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
+              tincidunt, leo non molestie consectetur, elit libero faucibus
+              tellus, sed fringilla tortor libero sit amet odio. Maecenas sed
+              ante condimentum mauris euismod pellentesque eu eu justo...
+            </p>
+            <div className="post-actions">
+              <button className="like-button" onClick={() => handleLike(index)}>
+                Like
+              </button>
+              <button
+                className="dislike-button"
+                onClick={() => handleDislike(index)}
+              >
+                Dislike
+              </button>
             </div>
           </div>
-          <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
-            tincidunt, leo non molestie consectetur, elit libero faucibus
-            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
-          </p>
-          <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
